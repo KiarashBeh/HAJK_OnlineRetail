22a62f107e0d055a1f7d7f7918c31225c46186a7
henrikjacobs <henrik.jacobss@gmail.com>
2018-03-23 14:34:04 +0100

    testing123

--- HAJK_OnlineRetail/Form1.cs
+++ HAJK_OnlineRetail/Form1.cs
@@ -16,5 +16,10 @@ namespace HAJK_OnlineRetail
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //testing 123
+        }
     }
 }
