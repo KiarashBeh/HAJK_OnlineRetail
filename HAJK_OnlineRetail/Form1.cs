72094945cd00d2bf246118533b8e9627c057c185
henrikjacobs <henrik.jacobss@gmail.com>
2018-03-26 09:32:05 +0200

    adrians test

--- HAJK_OnlineRetail/Form1.cs
+++ HAJK_OnlineRetail/Form1.cs
@@ -8,22 +8,112 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Data.SqlClient;
+using System.Windows.Forms.DataVisualization;
 
 namespace HAJK_OnlineRetail
 {
+
+
     public partial class Form1 : Form
     {
+        SqlConnection conn = new SqlConnection();
+
         public Form1()
         {
             InitializeComponent();
-        }
 
+            conn.ConnectionString = "Data Source=LAPTOP-7AL6OH88\\SQL2017;Initial Catalog=OnlineRetail;Integrated Security=True;Connection timeout=10";
+        }
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            InitData();
+            FirstChart();
+        }
 
+        private List<InvoiceRows> GetList()
+        {
+            List<InvoiceRows> fillOrderLines = new List<InvoiceRows>();
 
-            //testing 123
+            try
+            {
+                conn.Open();
+                SqlCommand myCommand = new SqlCommand("select * from OnlineRetail;", conn);
+
+                SqlDataReader myReader = myCommand.ExecuteReader();
+                MessageBox.Show("Connection fungerar");
+
+                int invoiceNum;
+                string stockCode;
+                string description;
+                int quantity;
+                DateTime invoiceDate;
+                float unitPrice;
+                int customerId;
+                string country;
+
+                while (myReader.Read())
+                {
+                    int.TryParse(myReader["invoiceNo"].ToString(), out invoiceNum);
+                    stockCode = myReader["StockCode"].ToString();
+                    description = myReader["Description"].ToString();
+                    int.TryParse(myReader["Quantity"].ToString(), out quantity);
+                    DateTime.TryParse(myReader["InvoiceDate"].ToString(), out invoiceDate);
+                    float.TryParse(myReader["UnitPrice"].ToString(), out unitPrice);
+                    int.TryParse(myReader["CustomerID"].ToString(), out customerId);
+                    country = myReader["Country"].ToString();
+
+                    InvoiceRows temRows = new InvoiceRows(
+                        invoiceNum, stockCode,
+                        description, quantity,
+                        invoiceDate, unitPrice,
+                        customerId, country);
+
+                    fillOrderLines.Add(temRows);
+                }
+            }
+            catch (Exception e)
+            {
+
+                MessageBox.Show(e.ToString());
+            }
+            finally
+            {
+                conn.Close();
+            }
+            return fillOrderLines;
+
+        }
+        private void InitData()
+        {
+            List<InvoiceRows> OrderLines1 = GetList();
+        }
+        private void FirstChart()
+        {
+
+            DateTime StartDate = DateTime.Parse(dateTimePicker1.Text);
+            DateTime EndDate = DateTime.Parse(dateTimePicker2.Text);
+
+            List<InvoiceRows> ChartList = GetList();
+
+            var datapoints = from asd in ChartList
+                             where asd.country == "Sweden"
+                             where asd.unitPrice > 0
+                             where asd.invoiceDate > StartDate
+                             where asd.invoiceDate < EndDate
+                             select new { sale = asd.quantity * asd.unitPrice, asd.invoiceDate };
+
+            foreach (var sales in datapoints)
+            {
+                chart1.Series["Series1"].Points.AddXY(sales.invoiceDate, sales.sale);
+            }
+            chart1.Series["Series1"].ChartType = SeriesChartType.Column;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FirstChart();
+
         }
     }
 }
